5f021278b01d9c4c738c0ac1ab9af944decb3c15
boublilm <45751927+boublilm@users.noreply.github.com>
2022-07-11 13:08:08 +0300

    Add files via upload

---
+++ piston_distance_calculator/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='piston_distance_calculator',
+      version='1.0',
+      description='Calculate the distance between 2 sticker on a piston',
+      author='Maor Boublil',
+      author_email='maorboublil@gmail.com',
+      url='https://github.com/boublilm/piston_distance_calculator',
+      packages=[],
+     )
